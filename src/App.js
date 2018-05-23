3a4e09765c9cb36313d0d5f10d360e44d6f0f8b7
Anthony Ward <pants1984@gmail.com>
2018-05-23 23:32:14 +0100

    some basic logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import finishes from './finishes'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      score: 180,
+      target: "Let's play"
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      score: event.target.value,
+      target: finishes.calculate(event.target.value)
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,8 +31,13 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          You have {this.state.score} remaining
         </p>
+
+          <input type="number" value={this.state.score} onChange={this.handleChange} />
+
+        <h3>{this.state.target}</h3>
+
       </div>
     );
   }
